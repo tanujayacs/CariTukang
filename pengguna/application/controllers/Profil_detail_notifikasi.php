ae77381a8692b9e9cc1ed95d6b4d1a78d9dd44b0
lukman <aljihadulukman@students.amikom.ac.id>
2025-01-17 21:28:39 +0700

    notif

---
+++ pengguna/application/controllers/Profil_detail_notifikasi.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profil_detail_notifikasi extends CI_Controller
+{
+    public function index() {
+
+        $this->load->view('header');
+        $this->load->view('profil/side_profil');
+        $this->load->view('profil/detail_notifikasi');
+        // $this->load->view('footer');
+        
+
+    }
+}
---
+++ pengguna/application/views/profil/detail_notifikasi.php
@@ -0,0 +1,52 @@
+<!-- CONTENT -->
+<div class="dashboard-content flex-grow-1">
+            <div class="d-flex mt-3">
+                <div class="status">
+                    <h3>Status Booking</h3>
+                    <span class="badge bg-primary">Selesai</span>
+                </div>
+                <div class="tgl text-end">
+                    <h5>Tanggal Booking:</h5>
+                    <h5>Selasa, 29 Februari 2025</h5>
+                </div>
+            </div>
+            <div class="container mt-4">
+                <div class="card p-3 shadow-sm">
+                    <!-- Bagian Atas: Gambar dan Detail Pesanan -->
+                    <div class="row">
+                        <!-- Gambar -->
+                        <div class="col-md-5">
+                            <img src="./assets/fotouser.png" alt="Tukang yang Anda Pesan"
+                                class="img-fluid rounded">
+                        </div>
+                        <!-- Detail Pesanan -->
+                        <div class="col-md-7 d-flex flex-column justify-content-center">
+                            <h5 class="fw-bold">Detail Booking</h5>
+                            <div>
+                                <h6 class="fw-bold mb-1">Tukang yang Anda Pesan</h6>
+                                <p class="mb-2">Tukang Listrik</p>
+                            </div>
+                            <div>
+                                <h6 class="fw-bold mb-1">Alamat Anda</h6>
+                                <p class="mb-2">Jl. Wijaya Kusuma, 03/04 Condong Catur, Kec. Depok, Kabupaten Sleman,
+                                    Yogyakarta</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Bagian Bawah: Deskripsi Masalah -->
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <h6 class="fw-bold mb-1">Deskripsi Masalah Anda</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur. Amet egestas lorem felis a sed id suspendisse. Id
+                                sit sit leo a urna aliquet. Congue massa eu elementum consequat risus magna. Et nec ut
+                                eget gravida.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex btn-card">
+                <button class="btn btn-primary flex-grow-1 m-3">Beri Ulasan</button>
+            </div>
+
+
+        </div>
